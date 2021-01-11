3f76c5e75caf8657f3d18019b6e0354e7aafa8f1
Samuel Pastva <daemontus@gmail.com>
2021-01-11 11:54:51 +0100

    CABEAN 2.0.0

---
+++ CABEAN-release-2.0.0/CaseStudy/Myeloid/myeloid.sh
@@ -0,0 +1,23 @@
+# Attractor detection
+./cabean -compositional 2 myeloid.ispl  
+
+# OI
+./cabean -compositional 2 -control OI -sin 5 -tin 4 myeloid.ispl 
+
+# OT
+./cabean -compositional 2 -control OT -sin 5 -tin 4 myeloid.ispl 
+
+# OP
+./cabean -compositional 2 -control OP -sin 5 -tin 4 myeloid.ispl
+
+# ASI
+./cabean -compositional 2 -control ASI -rmID rmID.txt -sin 5 -tin 4 myeloid.ispl 
+
+# AST
+./cabean -compositional 2 -control AST -rmID rmID.txt -sin 5 -tin 4 myeloid.ispl
+
+# ASP
+./cabean -compositional 2 -control ASP -rmID rmID.txt -sin 5 -tin 4 myeloid.ispl
+
+# TTC
+./cabean -compositional 2 -control TTC -tin 4 myeloid.ispl 
